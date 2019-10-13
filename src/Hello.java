45e3066c82ddb676f86560efb5c76e084229af81
zhangteng-tom <13753516426_zt@zt.com>
2019-10-12 23:38:22 -0400

    第一次提交

---
+++ src/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello word");
+        System.out.println("sayBy");
+        System.out.println("zhangteng");
+    }
+}
