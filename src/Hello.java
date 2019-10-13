a40154cfe32afa84276972049fe05db1d92ff89f
zhangteng-tom <13753516426_zt@zt.com>
2019-10-12 23:43:47 -0400

    第二 次提交

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello word");
         System.out.println("sayBy");
-        System.out.println("zhangteng");
+        System.out.println("    ");
+        System.out.println("/n");
+
     }
 }
